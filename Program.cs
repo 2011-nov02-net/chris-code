3cab7ec121544bc6c19eb77d469dcd4839a4dd3f
scrubbychris <cprbyron997@gmail.com>
2020-11-03 16:32:39 -0500

    Created new program to ask user for words and make an acronym out of it.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,14 @@ namespace practice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter words to make an acronym.");
+            string input = Console.ReadLine();
+            string [] output = input.Split(' ');
+
+            foreach (string first in output){
+                Console.Write(first[0]);
+            }
+            
         }
     }
 }
