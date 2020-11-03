499d4c7c71fc0dfb0176d688ee0a39518ca7c768
scrubbychris <cprbyron997@gmail.com>
2020-11-03 17:01:09 -0500

    First Push to Chris-Code Repo

---
+++ AcronymGenerator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter words to make an acronym.");
+            string input = Console.ReadLine();
+            string [] output = input.Split(' ');
+
+            foreach (string first in output){
+                Console.Write(first[0]);
+            }
+            
+        }
+    }
+}
